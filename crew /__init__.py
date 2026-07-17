23031ac7448a68c414e25c40f84f95c77717284c
Mohsen Mostafa <149787410+MohsenMostafa1@users.noreply.github.com>
2026-07-17 12:26:06 +0300

    Create __init__.py

---
+++ crew /__init__.py
@@ -0,0 +1,15 @@
+from agents.research_agents import (
+    academic_research_agent,
+    industrial_research_agent,
+    vacancy_scout_agent,
+    social_media_agent,
+    synthesis_agent,
+)
+
+__all__ = [
+    "academic_research_agent",
+    "industrial_research_agent",
+    "vacancy_scout_agent",
+    "social_media_agent",
+    "synthesis_agent",
+]
